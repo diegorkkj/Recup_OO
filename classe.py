a1f7ae22637b63687887e02572a658c852d8e9e1
diegorkkj <diegosenaisp@gmail.com>
2023-09-19 07:41:54 -0300

    up 2

--- classe.py
+++ classe.py
@@ -5,3 +5,19 @@ def limpar():
 
 def pausar():
     os.system("pause")
+
+class Cliente:
+    def __init__(self, nome, cpf, saldo):
+        self.nome = nome
+        self.cpf = cpf
+        self.saldo = 0
+
+class Banco:
+    def depositar():
+        pass
+
+    def sacar():
+        pass
+
+    def transferir():
+        pass
